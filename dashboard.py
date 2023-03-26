2e7ba87453740ecf8f5c3557ae3621a2266ca48e
Patrick-BOLOU <124937720+Patrick-BOLOU@users.noreply.github.com>
2023-03-26 23:06:31 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -13,7 +13,7 @@ def st_shap(plot, height=None):
     components.html(shap_html, height=height)
 
 st.write("Bienvenu dans l'application SCORING")
-X_test=pd.read_csv("X_test_real_data.csv",index_col='SK_ID_CURR')
+X_test=pd.read_csv("https://github.com/Patrick-BOLOU/Dashboard/blob/main/X_test_real_data.csv",index_col='SK_ID_CURR')
 
 liste_Id=joblib.load('lists_ID.joblib')
 expected_value=joblib.load('expected_value.joblib')
